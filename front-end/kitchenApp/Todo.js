f25932464f5ca01d8ddad1cd6a31e94ea99534b9
Hetpatel1234 <patel.het.d@gmail.com>
2020-03-02 17:37:17 -0600

    pushtest

---
+++ front-end/kitchenApp/Todo.js
@@ -0,0 +1,47 @@
+/*function Todos() {
+  const ref = firestore().collection('todos');
+
+  return (
+    <>
+      <Appbar>
+        <Appbar.Content title={'TODOs List'} />
+      </Appbar>
+      <ScrollView style={{flex: 1}}>
+        <Text>List of TODOs!</Text>
+      </ScrollView>
+      <TextInput label={'New Todo'} onChangeText={() => {}} />
+      <Button onPress={() => {}}>Add TODO</Button>
+    </>
+  );
+}*/
+import React from 'react';
+import firestore from '@react-native-firebase/firestore';
+import { List } from 'react-native-paper';
+
+
+
+
+function Todo ({ id, title, complete }) {
+      async function toggleComplete() {
+        await firestore()
+          .collection('kitchenApp')
+          .doc(id)
+          .update({
+            complete: !complete,
+          });
+      }
+
+      return (
+        <List.Item
+          title={title}
+          onPress={() => toggleComplete()}
+          left={props => (
+            <List.Icon {...props} icon={complete ? 'check' : 'cancel'} />
+          )}
+        />
+      );
+
+
+}
+
+export default React.memo(Todo);
