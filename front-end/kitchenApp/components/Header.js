b8b2f44864874cea189717656f3c56e37f883a22
simonkharel <47453356+simonkharel@users.noreply.github.com>
2020-03-13 14:43:31 -0500

    Update Header.js

--- front-end/kitchenApp/components/Header.js
+++ front-end/kitchenApp/components/Header.js
@@ -1,1 +1,27 @@
 
+import React, { Component } from 'react';
+import { StyleSheet, View,Text} from 'react-native';
+
+export default class Header extends Component{
+    render(){
+    return (
+        <View  style = {styles.header}>
+        <Text style = {styles.text}> Kitchen App</Text>
+        </View>
+    )
+}
+}
+
+const styles = StyleSheet.create({
+    header: {
+        height: 60,
+        backgroundColor: '#33ABF9',
+        padding: 15,
+    },
+    text: {
+        color: 'black',
+        fontSize: 25,
+        textAlign: 'center',
+    }
+}
+)
