0a24e8225549b6606c64fa49fe7c99bc5024100c
SMal95 <patriotsbestintheworld@hotmail.com>
2020-04-01 19:38:27 -0500

    Edited addToMenu, deleteFromMenu functions. Spell checking.

--- front-end/customerApp/src/menu_operations.js
+++ front-end/customerApp/src/menu_operations.js
@@ -47,50 +47,39 @@ export async function getMenu(type) {
     return items;
 }
 
-
 //function to add Menu item
 //parameter is a item object.
 //for example:
 /*let item = {
  allergen: ['Egg' , 'Milk'],
  calories: 750,
- indgredients: ['cheese', 'tomato sauce', 'pepperoni'],
+ ingredients: ['cheese', 'tomato sauce', 'pepperoni'],
  name: 'Pizza',
  price: 6.99,
  type: 'entree',
  uri: 'image link'
  }*/
  //then pass item to the function i.e, addToMenu(item)
-export async function addToMenu(item) {
-    let isSuccess;
-
-    await firebase.firestore().collection('Menu').add(item)
+export function addToMenu(item) {
+    firebase.firestore().collection('Menu').doc(item.name).set(item)
     .then(() => {
-        isSuccess = true;
+        console.log("Successfully added item to the menu.");
     })
     .catch((error) => {
-        console.error("Error adding item to menu: ", error);
-        isSuccess = false;
+        alert("Error adding item to menu: ", error);
     });
-
-    return isSuccess;
 }
 
 //function to delete a Menu item from the database
-//paramater is a string which is the menu item name i.e., deleteFromMenu('Pizza') will delete the menu item who's name is Pizza
-export async function deleteFromMenu(itemName) {
-    let isSuccess;
-
-    await firebase.firestore().collection('Menu').doc(itemName).delete()
+//parameter is a string which is the menu item name i.e., deleteFromMenu('Pizza') will delete the menu item who's name is Pizza
+export function deleteFromMenu(itemName) {
+    firebase.firestore().collection('Menu').doc(itemName).delete()
     .then(() => {
-        isSuccess = true;
+        console.log("Successfully deleted item from the menu.");
     })
     .catch((error) => {
-        console.error("Error deleting item from menu: ", error);
-        isSuccess = false;
+        alert("Error deleting item from menu: ", error);
     });
-
-    return isSuccess;
 }
 
 //function to update menu item infromation to the database
@@ -99,7 +88,7 @@ export async function deleteFromMenu(itemName) {
 /*let item = {
  allergen: ['Egg', 'Milk'],
  calories: 850,
- indgredients: ['blue cheese', 'tomato sauce', 'pineapple'],
+ ingredients: ['blue cheese', 'tomato sauce', 'pineapple'],
  name: 'Pizza',
  price: 7.99,
  type: 'entree',
@@ -110,7 +99,7 @@ export async function deleteFromMenu(itemName) {
 //the ingredients array will be changed to have the values ['blue cheese', 'tomato sauce', 'pineapple']
 //the name will remain the same
 //the price will be changed to be 7.99
-//the type will reamain entree
+//the type will remain entree
 //the uri can be changed to whatever image link you want
 export async function updateMenuItem(item) {
     let isSuccess;
@@ -128,7 +117,7 @@ export async function updateMenuItem(item) {
 }
 
 //function to get menu information from the database
-//paramater is a string which is the menu item name i.e., getItemDetails('Pizza') will get the menu item data for the menu item who's named is Pizza
+//parameter is a string which is the menu item name i.e., getItemDetails('Pizza') will get the menu item data for the menu item who's named is Pizza
 export async function getItemDetails(itemName) {
     let query;
 
@@ -144,15 +133,13 @@ export async function getItemDetails(itemName) {
     return query;
 }
 
-//function to check a particualar items inventory is not empty
+//function to check a particular items inventory is not empty
 //the parameter for this function is a string which is the name of the item i.e., checkInventory('Sugar')
 export async function checkInventory(name) {
     let query;
     await firebase.firestore().collection('Inventory').where('IngredientName', '==', name).get()
         .then(snapshot => {
-
             query = snapshot.docs.map(doc => doc.data());
-
         })
         .catch(error => {
             console.log('Error getting documents', error);
@@ -162,167 +149,9 @@ export async function checkInventory(name) {
     if (query == null) {
         return null;
     }
-    
 
-    
     if (query[0].IngredientQuantity == 0) {
         return 0;
     } else
         return 1;
-
-
 }
-
-//function to get all entrees when the inventory is not empty
-export async function getEntrees() {
-    let query;
-
-    await firebase.firestore().collection('Menu').where('type', '==', 'entree').get()
-        .then(snapshot => {
-            query = snapshot.docs.map(doc => doc.data());
-        })
-        .catch(error => {
-            console.log('Error getting documents', error);
-            query = null;
-        });
-    if (query == null) {
-        return query;
-    }
-    let availableItems = []
-    query.forEach((item, i) => {
-
-        let count = 1
-        item.ingredients.forEach((ingredient, j) => {
-
-            checkInventory(ingredient)
-                .then(snapshot => {
-
-                    query = snapshot;
-                })
-                .catch(error => {
-                    console.log('Error getting documents', error);
-                    query = null;
-                });
-
-            if (query == 0) {
-                count = 0;
-            }
-
-        });
-        if (count != 0) {
-            availableItems.push(item);
-        }
-    });
-
-    if (query == null) {
-        return null;
-    } else
-        return availableItems;
-
-
-}
-
-//function to get all beverages when the inventory is not empty
-export async function getBeverages() {
-    let query;
-
-    await firebase.firestore().collection('Menu').where('type', '==', 'beverage').get()
-        .then(snapshot => {
-            query = snapshot.docs.map(doc => doc.data());
-        })
-        .catch(error => {
-            console.log('Error getting documents', error);
-            query = null;
-        });
-    if (query == null) {
-        return query;
-    }
-    let availableItems = []
-    query.forEach((item, i) => {
-
-        let count = 1
-        item.ingredients.forEach((ingredient, j) => {
-
-            checkInventory(ingredient)
-                .then(snapshot => {
-
-                    query = snapshot;
-                })
-                .catch(error => {
-                    console.log('Error getting documents', error);
-                    query = null;
-                });
-
-            if (query == 0) {
-                count = 0;
-            }
-
-        });
-        if (count != 0) {
-            availableItems.push(item);
-        }
-    });
-
-    if (query == null) {
-        return null;
-    } else
-        return availableItems;
-
-
-}
-
-
- 
-//function to get all desserts when the inventory is not empty
-export async function getDesserts() {
-    let query;
-
-    await firebase.firestore().collection('Menu').where('type', '==', 'dessert').get()
-        .then(snapshot => {
-            query = snapshot.docs.map(doc => doc.data());
-        })
-        .catch(error => {
-            console.log('Error getting documents', error);
-            query = null;
-        });
-    if (query == null) {
-        return query;
-    }
-    let availableItems = [];
-    let isAvailable;
-    query.forEach((item, i) => {
-
-        let count = 1;
-        console.log(isAvailable)
-            
-        item.ingredients.forEach((ingredient, j) => {
-
-            checkInventory(ingredient)
-                .then(snapshot => {
-
-                    isAvailable = snapshot;
-                    console.log(isAvailable)
-                })
-                .catch(error => {
-                    console.log('Error getting documents', error);
-                    isAvailable = null;
-                });
-             console.log(isAvailable)
-             if (isAvailable == 0) {
-                 console.log('hello')
-                 count = 0;
-             }
-
-        });
-        if (count != 0) {
-            availableItems.push(item);
-        }
-    });
-
-    
-        return availableItems;
-
-
-}
-
-
