bfadcda2c28300c8d0d3218f5b5c9bb325ed9337
emf0072 <55302068+emf0072@users.noreply.github.com>
2020-04-07 00:54:15 -0500

    Add files via upload

--- front-end/managerApp/index.js
+++ front-end/managerApp/index.js
@@ -3,12 +3,8 @@
  * @format
  */
 
-/*import { AppRegistry } from 'react-native';
-import App from './App';
-import { name as appName } from './app.json';
-AppRegistry.registerComponent(appName, () => App);*/
 import { AppRegistry } from 'react-native';
-import Todos from './Todos';
+import App from './src//App';
 import { name as appName } from './app.json';
 
-AppRegistry.registerComponent(appName, () => Todos)
+AppRegistry.registerComponent(appName, () => App);
