798e80b2a2fd667b3c0a0d7e6ce4a0209bc8179b
aaronshehan <43185582+aaronshehan@users.noreply.github.com>
2020-04-10 20:19:27 -0500

    Update callServer.js

--- front-end/customerApp/src/callServer.js
+++ front-end/customerApp/src/callServer.js
@@ -3,3 +3,15 @@ import '@react-native-firebase/functions';
 import '@react-native-firebase/auth';
 import firestore from '@react-native-firebase/firestore'
 
+export async function login(email, password) {
+    let validEmail = true;
+    let customer;
+
+    await firebase.firestore().collection('callServer').where('email', '==', email).get()
+    .then((snapshot) => {
+        
+    })
+    .catch (error => {
+        console.log('Error calling server', error);
+    });
+}
