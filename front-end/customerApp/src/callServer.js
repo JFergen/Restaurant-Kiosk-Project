30996d8c59197d809602d4a5db19e9360b707950
aaronshehan <43185582+aaronshehan@users.noreply.github.com>
2020-04-10 20:14:47 -0500

    Create callServer.js

---
+++ front-end/customerApp/src/callServer.js
@@ -0,0 +1,5 @@
+import firebase from '@react-native-firebase/app';
+import '@react-native-firebase/functions';
+import '@react-native-firebase/auth';
+import firestore from '@react-native-firebase/firestore'
+
