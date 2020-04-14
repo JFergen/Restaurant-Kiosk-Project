c53ac224be890ac3f35606c562e4f65beb91711f
emf0072 <55302068+emf0072@users.noreply.github.com>
2020-04-13 21:35:41 -0500

    Add files via upload

--- front-end/managerApp/App.js
+++ front-end/managerApp/App.js
@@ -1,114 +1,152 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
+import Background from './assets/background.jpeg';
 import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+import {Button} from "react-native";
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import Home from './screens/HomeScreen';
+import Login from './screens/Login';
+import EmployeeManagement from './screens/employees/EmployeeManagement';
+import Inventory from './screens/inventory/InventoryManagement';
+import ViewInventory from './screens/inventory/viewInventory';
+import EditInventory from './screens/inventory/editInventory';
+import VendorGuide from './screens/inventory/vendorGuide';
+import NewEmployee from './screens/employees/newEmployee';
+import EditMenu from './screens/menu/editMenu';
+import ViewMenu from './screens/menu/viewMenu';
+import MenuManagement from './screens/menu/MenuManagement';
+import ApproveTimes from './screens/approveTimes/approveTimes';
+import ManageTips from './screens/manageTips/manageTips';
+import MetricsManagement from './screens/metrics/metricsManagement';
+import CompMeals from './screens/metrics/compMeals';
+import MetricsPage from './screens/metrics/MetricsPage';
+import TotalOrders from './screens/metrics/totalOrders';
 
-const App: () => React$Node = () => {
+const Stack = createStackNavigator();
+
+export function NavStack({ navigation }) {
   return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
+     <Stack.Navigator initialRouteName="Login">
+
+      <Stack.Screen
+        name="Login"
+        component={Login}
+        options={{ title: 'Login', headerStyle: {height: 70},
+                                   headerTitleAlign: 'center'}}
+      />
+
+      <Stack.Screen
+        name="Home"
+        component={Home}
+        options={{ title: 'Home' , headerStyle: {height: 70},
+                                   headerTitleAlign: 'center'}}
+
+      />
+      <Stack.Screen
+       name="EmployeeManagement"
+       component={EmployeeManagement}
+       options={{ title: 'EmployeeManagement' , headerStyle: {height: 70},
+                                                headerTitleAlign: 'center'}}
+      />
+      <Stack.Screen
+       name="Inventory"
+       component={Inventory}
+       options={{ title: 'Inventory' , headerStyle: {height: 70},
+                                       headerTitleAlign: 'center'}}
+      />
+
+      <Stack.Screen
+        name="ViewInventory"
+        component={ViewInventory}
+        options={{ title: 'View Current Inventory' , headerStyle: {height: 70},
+                                                     headerTitleAlign: 'center'}}
+      />
+
+      <Stack.Screen
+        name="EditInventory"
+        component={EditInventory}
+        options={{ title: 'Add/Edit Inventory' , headerStyle: {height: 70},
+                                                 headerTitleAlign: 'center'}}
+      />
+
+      <Stack.Screen
+        name="VendorGuide"
+        component={VendorGuide}
+        options={{ title: 'Vendor Resupply Guide' , headerStyle: {height: 70},
+                                                    headerTitleAlign: 'center'}}
+      />
+
+      <Stack.Screen
+        name="NewEmployee"
+        component={NewEmployee}
+        options={{ title: 'Add New Employee Form' , headerStyle: {height: 70},
+                                                    headerTitleAlign: 'center'}}
+      />
+
+      <Stack.Screen
+        name="MenuManagement"
+        component={MenuManagement}
+        options={{ title: 'Menu Management' , headerStyle: {height: 70},
+                                              headerTitleAlign: 'center'}}
+      />
+
+      <Stack.Screen
+        name="ViewMenu"
+        component={ViewMenu}
+        options={{ title: 'View Menu' , headerStyle: {height: 70},
+                                        headerTitleAlign: 'center'}}
+      />
+
+      <Stack.Screen
+        name="EditMenu"
+        component={EditMenu}
+        options={{ title: 'Edit Menu' , headerStyle: {height: 70},
+                                        headerTitleAlign: 'center'}}
+      />
+
+      <Stack.Screen
+        name="ApproveTimes"
+        component={ApproveTimes}
+        options={{ title: 'Approve Times' , headerStyle: {height: 70},
+                                            headerTitleAlign: 'center'}}
+      />
+
+      <Stack.Screen
+        name="ManageTips"
+        component={ManageTips}
+        options={{ title: 'Manage Tips' , headerStyle: {height: 70},
+                                          headerTitleAlign: 'center'}}
+      />
+
+      <Stack.Screen
+        name="CompMeals"
+        component={CompMeals}
+        options={{ title: 'Compensated Meals' , headerStyle: {height: 70},
+                                                headerTitleAlign: 'center'}}
+      />
+
+      <Stack.Screen
+        name="MetricsPage"
+        component={MetricsPage}
+        options={{ title: 'Restaurant Metrics' , headerStyle: {height: 70},
+                                                 headerTitleAlign: 'center'}}
+      />
+
+      <Stack.Screen
+        name="MetricsManagement"
+        component={MetricsManagement}
+        options={{ title: 'Metrics' , headerStyle: {height: 70},
+                                      headerTitleAlign: 'center'}}
+      />
+
+    </Stack.Navigator>
   );
-};
+}
 
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
-
-export default App;
+export default function App() {
+  return (
+    <NavigationContainer>
+      <NavStack />
+    </NavigationContainer>
+  );
+}
--- front-end/managerApp/index.js
+++ front-end/managerApp/index.js
@@ -3,12 +3,9 @@
  * @format
  */
 
-/*import { AppRegistry } from 'react-native';
+import 'react-native-gesture-handler';
+import { AppRegistry } from 'react-native';
 import App from './App';
 import { name as appName } from './app.json';
-AppRegistry.registerComponent(appName, () => App);*/
-import { AppRegistry } from 'react-native';
-import Todos from './Todos';
-import { name as appName } from './app.json';
 
-AppRegistry.registerComponent(appName, () => Todos)
+AppRegistry.registerComponent(appName, () => App);
