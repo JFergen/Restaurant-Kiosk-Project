5715cca288b12a4e9a28482c6bbd5dc44cc2755f
simonkharel <47453356+simonkharel@users.noreply.github.com>
2020-04-14 14:45:08 -0500

    Update AppNavigator.js

--- front-end/kitchenApp/Components/AppNavigator.js
+++ front-end/kitchenApp/Components/AppNavigator.js
@@ -1,25 +1,25 @@
-import {createStackNavigator,} from 'react-navigation-stack'
-import React, {Component} from 'react'
-import {AppRegistry} from 'react-native'
-import {createAppContainer} from 'react-navigation'
-import OrderContainer from './OrderContainer.js'
-import OrderDetails from './OrderDetails.js'
-import HomeScreen from './HomeScreen.js'
-import styles from '../Styles/Stylesheet.js' 
+import { createStackNavigator } from 'react-navigation-stack'
+import { React, Component } from 'react'
+import { AppRegistry } from 'react-native'
+import { createAppContainer } from 'react-navigation'
+import styles from '../Styles/Stylesheet.js'
+import TableScreen from './TableScreen.js'
 
+/* Currently not much navigation 
+needed in the app however, I kept this in place
+because it will be easy to add navigation with this setup in place
+if future adjustments are needed within the app. */
 const AppNavigator = createStackNavigator(
   {
- 
-  Home : HomeScreen,
-  Orders : OrderContainer,
-  Order_Details: OrderDetails,
+    // TableScreen is where all the components will be displayed.
+    Orders: TableScreen
   },
-  
-  {
-    initialRouteName: "Home"
-  }
 
-  
+  {
+    initialRouteName: 'Orders'
+  }
 )
+/* Going to be exporting a container comoponent and 
+calling it in App.js to display the app. */
 const AppContainer = createAppContainer(AppNavigator)
 export default AppContainer
