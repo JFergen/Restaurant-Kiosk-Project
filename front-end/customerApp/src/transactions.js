d689f372b30ca6faad57f280cd30239851d0827e
aaronshehan <43185582+aaronshehan@users.noreply.github.com>
2020-04-15 17:13:14 -0500

    Update transactions.js

--- front-end/customerApp/src/transactions.js
+++ front-end/customerApp/src/transactions.js
@@ -1,1 +1,28 @@
+import firebase from '@react-native-firebase/app';
+import '@react-native-firebase/functions';
+import '@react-native-firebase/auth';
+import firestore from '@react-native-firebase/firestore'
 
+export async function addTransaction(transaction) {
+    let isSuccess;
+    let errorMessage;
+  
+    await firebase.firestore().collection('Transactions').add(transaction)
+    .then(() => {
+        console.log("Successfully added transaction.");
+        isSuccess = true;
+    })
+    .catch((error) => {
+        alert("Error adding table to table doc: ", error);
+        isSuccess = false;
+        errorMessage = error;
+    });
+  
+    if (!isSuccess) {
+      return errorMessage;
+    }
+  
+     return true;
+
+    
+}
