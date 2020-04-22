225961e8ca14ec8966076f79858c19a27172c4e5
aaronshehan <43185582+aaronshehan@users.noreply.github.com>
2020-04-22 11:23:29 -0500

    Update transactions.js

--- front-end/customerApp/src/transactions.js
+++ front-end/customerApp/src/transactions.js
@@ -26,10 +26,42 @@ export async function addTransaction(transaction) {
         isSuccess = false;
         errorMessage = error;
     });
+    
+   let dailyRev;
+    
+    await firebase.firestore().collection('DailyRevenue').doc('2020')
+    .then((snapshot) => {
+        console.log("Successfully retreived daily revenue");
+        isSuccess = true;
+        dailyRev = snapshot.docs.map(doc => doc.data());
+    })
+    .catch((error) => {
+        alert("Error retreiving daily revenue ", error);
+        isSuccess = false;
+        errorMessage = error;
+    });
   
+    dailyRev = dailyRev[0].April[21];
+    dailyRev += parseFloat(transaction.orderTotal);
+        
+    let arr = Array.apply(0, Array(21)).map(function () {});
+    arr[21] = dailyRev;
+        
+     await firebase.firestore().collection('DailyRevenue').doc('2020').set({April: dailyRev )
+    .then(() => {
+        console.log("Employee added Successfully");
+        isSuccess = true;
+    })
+    .catch((error) => {
+        console.error("Error adding Employee to Employee table: ", error);
+        isSuccess = false;
+    });
+        
     if (!isSuccess) {
       return errorMessage;
     }
+    
+    
   
      return true;
 }
